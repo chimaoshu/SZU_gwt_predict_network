c92dd5a632a7377306c1bfdbfeb42d4a6ec7a964
Red_Hairy_Mouse <56833537+cjw-bot@users.noreply.github.com>
2020-06-27 23:48:41 +0800

    添加注释

--- 数据处理/GetPopularData.py
+++ 数据处理/GetPopularData.py
@@ -1,15 +1,19 @@
 """
 这个脚本的目的是把数据中出现频率前600名的关键词跳出来
-出现频率前600名的关键词，由TestDataDistribution.py脚本可以
+由TestDataDistribution.py脚本可以知道前600名的关键词正好就是出现次数不小于200的公文
 """
+
 import json
-with open('data\\word_weights.json', 'r', encoding='utf-8') as f:
+# 读取数据
+with open('word_weights.json', 'r', encoding='utf-8') as f:
     _data = json.loads(f.read())
 
 new_data_list = {}
+# 找到并存储出现此处超过200的关键词
 for keyword in _data.keys():
     if _data[keyword]['total_doc_num'] >= 200:
         new_data_list[keyword] = _data[keyword]
 
-with open('data\\popular_keyword_data.json', 'a+', encoding='utf-8') as f:
+# 写入数据
+with open('popular_keyword_data.json', 'a+', encoding='utf-8') as f:
     f.write(json.dumps(new_data_list, ensure_ascii=False))
--- 数据处理/TestDataDistribution.py
+++ 数据处理/TestDataDistribution.py
@@ -4,16 +4,21 @@
 我们就采用这600个关键词来做神经元的输入层，用0和1表示关键词的出现与否
 """
 import json
+
+# 读取数据
 with open('data\\word_weights.json', 'r', encoding='utf-8') as f:
     data = f.read()
     data = json.loads(data)
 
 while 1:
+    # 输入数字
     max_num = input('输入数字：')
     total_num = 0
+
+    # 遍历检查，计算出现次数不小于输入数字的公文数目
     for keyword in data.keys():
         if data[keyword]['total_doc_num'] >= int(max_num):
             total_num += 1
 
-    print('几年以来公文数目超过%s的关键词的总数为%s\n' % (max_num, total_num))
+    print('几年以来出现次数超过%s的关键词的总数为%s\n' % (max_num, total_num))
 
